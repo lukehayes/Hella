27d6c7bfc25da75df596829256442a2963103c05
lukehayes <lukepurplehayes@gmail.com>
2023-09-10 16:27:47 +0100

    Corrected Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
     public function new()
     {
         super();
-        addChild(new FlxGame(256, 144, PlayState, 60,60, true));
+        addChild(new FlxGame(256, 144, levels.DebugLevel, 60,60, true));
     }
 }
